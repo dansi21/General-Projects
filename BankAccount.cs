0e8a4f7dc9723e46c6f19587fd0a6b1ed52a1078
dansi21 <willguy21@gmail.com>
2018-12-29 22:21:02 -0800

    Main Parent Bank Account V1

---
+++ BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Accounts
+{
+
+    class BankAccount
+    {
+        protected double Balance;
+        protected String AccountName;
+        protected bool frozenStatus;
+
+        public BankAccount(String name = "Unspecified", double balanceIn = 0, bool status = false) {
+            Balance = balanceIn;
+            AccountName = name;
+            frozenStatus = status;
+        }
+
+        public double getBalance() {
+            return Balance;
+        }
+
+        public String getName() {
+            return AccountName;
+        }
+
+        public bool Status() {
+            return frozenStatus;
+        }
+
+        public void Withdraw(double x) {
+            if (!statusHandler(x))
+            {
+                Balance = Balance - x;
+            }
+            else {
+                Console.WriteLine("You Are Attempting A Large Withdrawl. Account Frozen");
+                Console.WriteLine("Please Contact A Branch Location");
+            }
+        }
+
+        public void Deposit(double x) {
+            if (!statusHandler(x)){
+                Balance = Balance + x;
+            }
+            else
+            {
+                Console.WriteLine("You Are Attempting A Large Deposit. Account Frozen");
+                Console.WriteLine("Please Contact A Branch Location");
+            }
+        }
+
+        public void unfreeze() {
+            frozenStatus = false;
+        }
+
+        protected bool statusHandler(double x) {
+            if (x > (0.5 * Balance) || x > 2500) {//Currently all transations on an empty account freeze said account
+                frozenStatus = true;
+            }
+            return frozenStatus;
+        }
+
+        //overload <<
+
+    }
+}
