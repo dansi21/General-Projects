9749041508119bbe5258808b1d7f9a775417a31a
dansi21 <willguy21@gmail.com>
2018-12-30 18:30:31 -0800

    Introductory T2 Account With Higher Freeze Limit

---
+++ Tier2Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    class Tier2Account:BankAccount
+    {
+        protected override bool statusHandler(double x)
+        {
+            if (x > 25000)
+            {
+                frozenStatus = true;
+            }
+            return frozenStatus;
+        }
+    }
+}
